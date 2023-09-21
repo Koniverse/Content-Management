c695bc36f0f6e52dbff16ba3530f5db7d37bc354
AnhMTV <maithachvietanh@gmail.com>
2023-09-21 16:31:03 +0700

    The first config with some basic config

---
+++ src/api/dapp/controllers/dapp.ts
@@ -0,0 +1,7 @@
+/**
+ * dapp controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::dapp.dapp');
---
+++ src/api/dapp/routes/dapp.ts
@@ -0,0 +1,7 @@
+/**
+ * dapp router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::dapp.dapp');
