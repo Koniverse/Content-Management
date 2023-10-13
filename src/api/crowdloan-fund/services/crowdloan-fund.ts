f9d1c8f376b84dafd2908c408a88eb1618bbff45
AnhMTV <maithachvietanh@gmail.com>
2023-10-13 18:08:03 +0700

    Update period calculator 2

--- src/api/crowdloan-fund/services/crowdloan-fund.ts
+++ src/api/crowdloan-fund/services/crowdloan-fund.ts
@@ -38,7 +38,7 @@ const STATUS_MAP: Record<number, 'in_auction' | 'won' | 'withdraw' | 'failed'> =
 }
 
 const periodMs = 12 * 7 * 24 * 60 * 60 * 1000;
-const periodKMs = 12 * 7 * 24 * 60 * 60 * 1000;
+const periodKMs = 6 * 7 * 24 * 60 * 60 * 1000;
 const RELAY_DATE_MAP = {
   polkadot: (new Date('2021-12-21T01:48:00')).getTime() - (6 * periodMs),
   kusama: (new Date('2021-09-08T04:27:00')).getTime() - (15 * periodKMs),
