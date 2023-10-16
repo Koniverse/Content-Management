5af289fbcbcf7ac323d01fafc345f5bb3dcd162d
AnhMTV <maithachvietanh@gmail.com>
2023-10-16 15:01:52 +0700

    Add support chains to the airdrop chain list api

--- src/api/airdrop-campaign/services/airdrop-campaign.ts
+++ src/api/airdrop-campaign/services/airdrop-campaign.ts
@@ -7,12 +7,14 @@ import {factories} from '@strapi/strapi';
 export default factories.createCoreService('api::airdrop-campaign.airdrop-campaign', ({strapi}) => ({
   async customList(params= {}) {
     const data = await strapi.entityService.findMany('api::airdrop-campaign.airdrop-campaign', {
-      populate: ['logo', 'backdrop_image'],
+      populate: ['logo', 'backdrop_image', 'chains'],
       sort: 'ordinal:asc,id:asc',
       ...params
     })
 
     data.forEach((d) => {
+      // @ts-ignore
+      d.chains = d.chains?.map((c) => (c.slug)) || [];
       d.logo = d.logo?.url || d.logo;
       d.backdrop_image = d.backdrop_image?.url || d.backdrop_image;
     })
